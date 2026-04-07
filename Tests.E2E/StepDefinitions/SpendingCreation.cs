c6a27b00e3058dc48b11726fdec37e1d995ceb6f
D Yermachenko <d.yermachenko@gmail.com>
2026-04-08 01:08:11 +0200

    Adding step definitions

---
+++ Tests.E2E/StepDefinitions/SpendingCreation.cs
@@ -0,0 +1,46 @@
+﻿using Reqnroll;
+
+namespace Tests.E2E.StepDefinitions;
+
+[Binding]
+public sealed class SpendingCreation(Reqnroll.IReqnrollOutputHelper outputHelper)
+{
+    [Given("I am on the spending creation page")]
+    public void GivenIAmOnTheSpendingCreationPage()
+    {
+        outputHelper.WriteLine("Navigating to the spending creation page...");
+    }
+
+    [When("I click on the {string} button")]
+    public void WhenIClickOnTheButton(string p0)
+    {
+        outputHelper.WriteLine($"Clicking on the '{p0}' button...");
+    }
+
+    [When("I fill in the form with valid data")]
+    public void WhenIFillInTheFormWithValidData(DataTable dataTable)
+    {
+        outputHelper.WriteLine("Filling in the form with valid data...");
+    }
+
+    [When("I submit the form")]
+    public void WhenISubmitTheForm()
+    {
+        outputHelper.WriteLine("Submitting the form...");
+    }
+
+    [Then("I should see a success message")]
+    public void ThenIShouldSeeASuccessMessage()
+    {
+        outputHelper.WriteLine("Checking for success message...");
+        Assert.True(true, "Success message is displayed.");
+    }
+
+    [Then("the new spending should be listed in the datatable")]
+    public void ThenTheNewSpendingShouldBeListedInTheDatatable()
+    {
+        outputHelper.WriteLine("Checking if the new spending is listed in the datatable...");
+        Assert.True(true, "New spending is listed in the datatable.");
+    }
+
+}
